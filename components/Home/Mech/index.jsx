83ee2bd81c6089d134a3ab74a5b5e8843738c9e0
mohandast52 <mohandast52@gmail.com>
2023-05-15 18:17:08 +0530

    request table udpated

--- components/Home/Mech/index.jsx
+++ components/Home/Mech/index.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import Web3 from 'web3';
+import { Table } from 'antd/lib';
 import { AGENT_MECH_ABI } from 'common-util/AbiAndAddresses';
 import Request from './components/Request';
 
@@ -7,9 +8,7 @@ import Request from './components/Request';
 const CONTRACT_ADDRESS = '0xFf82123dFB52ab75C417195c5fDB87630145ae81';
 const WEBSOCKET_PROVIDER = 'wss://rpc.gnosischain.com/wss';
 
-const truncate = (str, length) => (
-  str && str.length > length ? `${str.substring(0, length)}...` : str
-);
+const truncate = (str, length) => (str && str.length > length ? `${str.substring(0, length)}...` : str);
 
 const cellStyle = {
   padding: '8px',
@@ -22,13 +21,18 @@ const EventListener = () => {
   const [secondEvents, setSecondEvents] = useState([]);
 
   useEffect(() => {
-    const web3Instance = new Web3(new Web3.providers.WebsocketProvider(WEBSOCKET_PROVIDER));
+    const web3Instance = new Web3(
+      new Web3.providers.WebsocketProvider(WEBSOCKET_PROVIDER),
+    );
     setWeb3Ws(web3Instance);
   }, []);
 
   useEffect(() => {
     if (web3Ws) {
-      const contractInstance = new web3Ws.eth.Contract(AGENT_MECH_ABI, CONTRACT_ADDRESS);
+      const contractInstance = new web3Ws.eth.Contract(
+        AGENT_MECH_ABI,
+        CONTRACT_ADDRESS,
+      );
       setContractWs(contractInstance);
     }
   }, [web3Ws]);
@@ -85,10 +89,64 @@ const EventListener = () => {
     }
   }, [contractWs]);
 
+  const getDatasource = (eventsPassed) => eventsPassed.map((event, index) => ({
+    key: `row-request-${index}`,
+    index: index + 1,
+    requestId: event.returnValues.requestId,
+    sender: event.returnValues.sender,
+    // sender: truncate(event.returnValues.sender, 10),
+    data: event.returnValues.data,
+  }));
+
+  const requestsDatasource = getDatasource(firstEvents);
+
   return (
     <div>
       <Request />
       <h2>Requests</h2>
+
+      <Table
+        columns={[
+          {
+            title: 'Index',
+            dataIndex: 'index',
+            key: 'index',
+            render: (text) => <span>{text}</span>,
+          },
+          {
+            title: 'Request Id',
+            dataIndex: 'requestId',
+            key: 'requestId',
+            render: (text) => <span>{text}</span>,
+          },
+          {
+            title: 'Sender',
+            dataIndex: 'sender',
+            key: 'sender',
+            render: (text) => <span>{text}</span>,
+          },
+          {
+            title: 'Data',
+            dataIndex: 'data',
+            key: 'data',
+            render: (text) => <span>{text}</span>,
+          },
+        ]}
+        dataSource={requestsDatasource}
+        pagination={false}
+        // pagination={
+        //   isPaginationRequired
+        //     ? {
+        //       total,
+        //       current: currentPage,
+        //       defaultPageSize: TOTAL_VIEW_COUNT,
+        //       onChange: (e) => setCurrentPage(e),
+        //     }
+        //     : false
+        // }
+        scroll={{ x: 1200 }}
+        rowKey={(x) => x.key}
+      />
       <table>
         <thead>
           <tr>
@@ -103,7 +161,9 @@ const EventListener = () => {
             <tr key={index}>
               <td style={cellStyle}>{index}</td>
               <td style={cellStyle}>{event.returnValues.requestId}</td>
-              <td style={cellStyle}>{truncate(event.returnValues.sender, 10)}</td>
+              <td style={cellStyle}>
+                {truncate(event.returnValues.sender, 10)}
+              </td>
               <td style={cellStyle}>{event.returnValues.data}</td>
             </tr>
           ))}
@@ -124,7 +184,9 @@ const EventListener = () => {
             <tr key={index}>
               <td style={cellStyle}>{index}</td>
               <td style={cellStyle}>{event.returnValues.requestId}</td>
-              <td style={cellStyle}>{truncate(event.returnValues.sender, 10)}</td>
+              <td style={cellStyle}>
+                {truncate(event.returnValues.sender, 10)}
+              </td>
               <td style={cellStyle}>{event.returnValues.data}</td>
             </tr>
           ))}
