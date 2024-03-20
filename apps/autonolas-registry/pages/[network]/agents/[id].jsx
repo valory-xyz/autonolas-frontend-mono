623c36d4bdac20e6a4260560a0406fb119020e6a
mohandast52 <mohandast52@gmail.com>
2024-03-20 18:03:51 +0530

    change import

--- apps/autonolas-registry/pages/[network]/agents/[id].jsx
+++ apps/autonolas-registry/pages/[network]/agents/[id].jsx
@@ -1,6 +1,6 @@
 import dynamic from 'next/dynamic';
 
-const AgentDetails = dynamic(() => import('components/ListAgents/details'), {
+const AgentDetails = dynamic(() => import('../../../components/ListAgents/details'), {
   ssr: false,
 });
 
