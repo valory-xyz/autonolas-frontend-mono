726d38dc6b7b665a9962ff9b500443ff7dbd0ef4
mohandast52 <mohandast52@gmail.com>
2024-04-24 18:14:22 +0530

    tiny

--- apps/tokenomics/common-util/constants/numbers.js
+++ apps/tokenomics/common-util/constants/numbers.js
@@ -1,6 +1,6 @@
-import { constants } from 'ethers';
+import { ethers } from 'ethers';
 
-export const ADDRESS_ZERO = constants.AddressZero;
+export const ADDRESS_ZERO = ethers.ZeroAddress;
 
-export const ONE_ETH = constants.WeiPerEther;
+export const ONE_ETH = ethers.WeiPerEther;
 export const ONE_ETH_IN_STRING = ONE_ETH.toString();
