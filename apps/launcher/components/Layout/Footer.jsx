b3b8b25e8be953219b62fef287192554ef9585a0
mohandast52 <mohandast52@gmail.com>
2024-05-06 18:14:07 +0530

    remove contract infos

--- apps/launcher/components/Layout/Footer.jsx
+++ apps/launcher/components/Layout/Footer.jsx
@@ -2,9 +2,8 @@ import Image from 'next/image';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 
-import { Footer as CommonFooter, getExplorerURL } from '@autonolas/frontend-library';
+import { Footer as CommonFooter } from '@autonolas/frontend-library';
 
-import { ADDRESSES } from 'common-util/Contracts/addresses';
 import { useHelpers } from 'common-util/hooks';
 import { PAGES_TO_LOAD_WITHOUT_CHAINID } from 'util/constants';
 
@@ -12,58 +11,13 @@ import Socials from './Socials';
 import { ContractsInfoContainer } from './styles';
 
 const ContractInfo = () => {
-  const { chainId, isL1Network, doesNetworkHaveValidServiceManagerToken } = useHelpers();
+  const { chainId } = useHelpers();
   const router = useRouter();
 
   const { pathname } = router;
 
   // if chainId is not set, show empty container
   if (!chainId) return <ContractsInfoContainer />;
-
-  const addresses = ADDRESSES[chainId];
-  const getCurrentPageAddresses = () => {
-    if (addresses && (pathname || '').includes('components')) {
-      return {
-        registryText: 'ComponentRegistry',
-        managerText: 'RegistriesManager',
-        registry: addresses.componentRegistry,
-        manager: addresses.registriesManager,
-      };
-    }
-
-    if (addresses && (pathname || '').includes('agents')) {
-      return {
-        registryText: 'AgentRegistry',
-        managerText: 'RegistriesManager',
-        registry: addresses.agentRegistry,
-        manager: addresses.registriesManager,
-      };
-    }
-
-    if (addresses && (pathname || '').includes('services')) {
-      return {
-        registryText: 'ServiceRegistry',
-        managerText: 'ServiceManager',
-        registry: isL1Network ? addresses.serviceRegistry : addresses.serviceRegistryL2,
-        manager: doesNetworkHaveValidServiceManagerToken
-          ? addresses.serviceManagerToken
-          : addresses.serviceManager,
-      };
-    }
-
-    return {
-      registry: null,
-      manager: null,
-      registryText: null,
-      managerText: null,
-    };
-  };
-
-  const getContractInfo = (text, addressToPoint) => (
-    <div className="registry-contract">&nbsp;•&nbsp; Contract Info</div>
-  );
-
-  const { registry, manager, managerText, registryText } = getCurrentPageAddresses();
 
   return (
     <ContractsInfoContainer>
@@ -73,8 +27,6 @@ const ContractInfo = () => {
             <Image src="/images/etherscan-logo.svg" width={18} height={18} alt="Etherscan link" />
             <span>Contracts</span>
           </div>
-          {getContractInfo(registryText, registry)}
-          {getContractInfo(managerText, manager)}
         </>
       )}
     </ContractsInfoContainer>
