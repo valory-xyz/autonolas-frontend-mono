d860584a01cc0e89419104a30a39fcacb5dd3eda
Atatakai <atatakai@Atatakais-MacBook-Pro.local>
2024-06-14 18:13:56 +0400

    Fix bond deploy

--- apps/bond/common-util/functions/index.js
+++ apps/bond/common-util/functions/index.js
@@ -1,7 +1,6 @@
 export * from './addresses';
 export * from './chains';
 export * from './errors';
-export * from './requests'
 export * from './ethers';
 export * from './frontend-library';
 export * from './web3';
