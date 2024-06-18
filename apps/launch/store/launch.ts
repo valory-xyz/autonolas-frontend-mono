353d7069da6b7f6a183065a40b2d58eccbde8370
mohandast52 <mohandast52@gmail.com>
2024-06-19 02:36:52 +0530

    refactor: Update launch store to include my staking contracts

--- apps/launch/store/launch.ts
+++ apps/launch/store/launch.ts
@@ -1,16 +1,30 @@
+import { PayloadAction } from '@reduxjs/toolkit';
 import { createSlice } from '@reduxjs/toolkit';
 
-interface LaunchState {}
+import { MyStakingContract } from 'types/index';
 
-const initialState: LaunchState = {};
+type LaunchState = {
+  isMyStakingContractsLoading: boolean;
+  /** list of my staking contract */
+  myStakingContracts: MyStakingContract[];
+};
+
+const initialState: LaunchState = {
+  isMyStakingContractsLoading: true,
+  myStakingContracts: [],
+};
 
 export const launchSlice = createSlice({
   name: 'launch',
   initialState,
   reducers: {
+    setStakingContracts: (state, action: PayloadAction<LaunchState['myStakingContracts']>) => {
+      state.myStakingContracts = action.payload;
+      state.isMyStakingContractsLoading = false;
+    },
     clearState: (state) => {},
   },
 });
 
-export const { clearState } = launchSlice.actions;
+export const { setStakingContracts, clearState } = launchSlice.actions;
 export const launchReducer = launchSlice.reducer;
