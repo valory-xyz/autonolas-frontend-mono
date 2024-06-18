d95dcfed83ee48208042cc17991fca5b4e86ba61
mohandast52 <mohandast52@gmail.com>
2024-06-19 04:16:13 +0530

    feat: Add dummy data for my staking contracts in launch store

--- apps/launch/store/launch.ts
+++ apps/launch/store/launch.ts
@@ -9,9 +9,29 @@ type LaunchState = {
   myStakingContracts: MyStakingContract[];
 };
 
+const dummyMyStakingContracts: MyStakingContract[] = [
+  {
+    id: '1',
+    name: 'Contract #1 Create Prediction Market Modernized',
+    description:
+      'This is the very long description of the contract that targets governors and explains the value of the contract to them. This is the very long description of the contract that targets governors and explains the value of the contract to them.',
+    template: 'Create Prediction Markets',
+    isNominated: true,
+  },
+  {
+    id: '2',
+    name: 'Contract #2 Create Prediction Market Modernized',
+    description:
+      'lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    template: 'Create Prediction Markets',
+    isNominated: false,
+  },
+];
+
 const initialState: LaunchState = {
-  isMyStakingContractsLoading: true,
-  myStakingContracts: [],
+  isMyStakingContractsLoading: false,
+  myStakingContracts: dummyMyStakingContracts,
+  // myStakingContracts: [],
 };
 
 export const launchSlice = createSlice({
