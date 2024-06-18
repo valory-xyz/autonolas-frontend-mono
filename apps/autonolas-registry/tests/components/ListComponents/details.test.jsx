9a3999d8b6cd7badcba755606a6afffdf9da1545
mohandast52 <mohandast52@gmail.com>
2024-06-18 14:20:51 +0530

    refactor: Update import paths for components and hooks in autonolas-registry tests and common-util

--- apps/autonolas-registry/tests/components/ListComponents/details.test.jsx
+++ apps/autonolas-registry/tests/components/ListComponents/details.test.jsx
@@ -1,23 +1,24 @@
 import { render, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 
-import { GATEWAY_URL } from '../../../util/constants';
-import Component from '../../../components/ListComponents/details';
+import Component from 'components/ListComponents/details';
 import {
   getComponentDetails,
   getComponentHashes,
   getComponentOwner,
   getTokenUri,
-} from '../../../components/ListComponents/utils';
+} from 'components/ListComponents/utils';
+import { GATEWAY_URL } from 'util/constants';
+
 import {
   dummyAddress,
-  wrapProvider,
-  mockV1Hash,
-  mockNftImageHash,
-  mockIpfs,
   mockCodeUri,
+  mockIpfs,
+  mockNftImageHash,
+  mockV1Hash,
   svmConnectivityEmptyMock,
   useHelpersEvmMock,
+  wrapProvider,
 } from '../../tests-helpers';
 
 jest.mock('next/router', () => ({
@@ -27,26 +28,33 @@ jest.mock('next/router', () => ({
   },
 }));
 
-jest.mock('../../../common-util/List/IpfsHashGenerationModal/helpers', () => ({
+jest.mock('wagmi', () => ({
+  __esModule: true,
+  useEnsName() {
+    return { data: 'ENS Value' };
+  },
+}));
+
+jest.mock('common-util/List/IpfsHashGenerationModal/helpers', () => ({
   getIpfsHashHelper: jest.fn(() => mockV1Hash),
 }));
 
-jest.mock('../../../common-util/Details/utils', () => ({
+jest.mock('common-util/Details/utils', () => ({
   checkIfServiceRequiresWhitelisting: jest.fn(() => false),
 }));
 
-jest.mock('../../../components/ListComponents/utils', () => ({
+jest.mock('components/ListComponents/utils', () => ({
   getComponentDetails: jest.fn(),
   getComponentHashes: jest.fn(),
   getComponentOwner: jest.fn(),
   getTokenUri: jest.fn(),
 }));
 
-jest.mock('../../../common-util/hooks/useHelpers', () => ({
+jest.mock('common-util/hooks/useHelpers', () => ({
   useHelpers: () => useHelpersEvmMock,
 }));
 
-jest.mock('../../../common-util/hooks/useSvmConnectivity', () => ({
+jest.mock('common-util/hooks/useSvmConnectivity', () => ({
   useSvmConnectivity: () => svmConnectivityEmptyMock,
 }));
 
@@ -84,43 +92,29 @@ describe('listComponents/details.jsx', () => {
   });
 
   it('should display component details', async () => {
-    const { getByText, queryByRole, getByTestId } = render(
-      wrapProvider(<Component />),
-    );
+    const { getByText, queryByRole, getByTestId } = render(wrapProvider(<Component />));
     await waitFor(async () => {
       // left column content
       expect(getByText('Some package name')).toBeInTheDocument();
-      expect(getByTestId('view-hash-link').getAttribute('href')).toBe(
-        `${GATEWAY_URL}12345`,
-      );
+      expect(getByTestId('view-hash-link').getAttribute('href')).toBe(`${GATEWAY_URL}12345`);
       expect(getByTestId('view-code-link').getAttribute('href')).toBe(
         `${GATEWAY_URL}${mockCodeUri}`,
       );
       expect(getByTestId('description').textContent).toBe(mockIpfs.description);
-      expect(getByTestId('version').textContent).toBe(
-        mockIpfs.attributes[0].value,
-      );
-      expect(getByTestId('owner-address').textContent).toBe(
-        dummyDetails.developer,
-      );
-      expect(
-        queryByRole('button', { name: 'Update Hash' }),
-      ).toBeInTheDocument();
+      expect(getByTestId('version').textContent).toBe(mockIpfs.attributes[0].value);
+      expect(getByTestId('owner-address').textContent).toBe(dummyDetails.developer);
+      expect(queryByRole('button', { name: 'Update Hash' })).toBeInTheDocument();
       expect(getByTestId('details-dependency')).toBeInTheDocument();
 
       // NFT image
       const nftImage = getByTestId('nft-image').querySelector('.ant-image-img');
-      expect(nftImage.getAttribute('src')).toBe(
-        `${GATEWAY_URL}${mockNftImageHash}`,
-      );
+      expect(nftImage.getAttribute('src')).toBe(`${GATEWAY_URL}${mockNftImageHash}`);
     });
   });
 
   it('should display update hash button only for owner and open the modal when clicked', async () => {
     getComponentOwner.mockResolvedValue(dummyAddress);
-    const { findByRole, findByText, queryByText } = render(
-      wrapProvider(<Component />),
-    );
+    const { findByRole, findByText, queryByText } = render(wrapProvider(<Component />));
     const updateHashButton = await findByRole('button', {
       name: 'Update Hash',
     });
