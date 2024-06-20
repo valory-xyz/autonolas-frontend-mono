1f8672e26cd8e0cbd4f5c5d4a2dbcfa261f00d80
mohandast52 <mohandast52@gmail.com>
2024-06-20 15:46:52 +0530

    refactor: Hide menu on certain pages in Layout component

--- apps/launch/components/Layout/index.tsx
+++ apps/launch/components/Layout/index.tsx
@@ -1,9 +1,11 @@
 import { Layout as AntdLayout } from 'antd';
 import dynamic from 'next/dynamic';
+import { useRouter } from 'next/router';
 import { FC, ReactNode } from 'react';
 
-import { LoginV2 } from '../Login';
+import { URL } from 'common-util/constants/urls';
 
+import { LoginV2 } from '../Login';
 import { LogoSvg } from './Logos';
 import { SwitchNetworkSelect } from './SwitchNetworkSelect';
 import { CustomLayout, Logo, OlasHeader, RightMenu } from './styles';
@@ -17,21 +19,28 @@ interface LayoutProps {
   children?: ReactNode;
 }
 
+const pagesWithoutMenu = [URL.nominateContract];
+
 const Layout: FC<LayoutProps> = ({ children }) => {
+  const router = useRouter();
+  const isMenuHidden = pagesWithoutMenu.some((page) => router.pathname.includes(page));
+
   return (
     <CustomLayout>
-      <OlasHeader>
-        <Logo href="/">
-          <LogoSvg />
-        </Logo>
-        <NavigationMenu />
-        <RightMenu>
-          <SwitchNetworkSelect />
-          <LoginV2 />
-        </RightMenu>
-      </OlasHeader>
+      {isMenuHidden ? null : (
+        <OlasHeader>
+          <Logo href="/">
+            <LogoSvg />
+          </Logo>
+          <NavigationMenu />
+          <RightMenu>
+            <SwitchNetworkSelect />
+            <LoginV2 />
+          </RightMenu>
+        </OlasHeader>
+      )}
 
-      <Content className="site-layout">
+      <Content className={`site-layout ${isMenuHidden ? 'mt-24' : ''}`}>
         <div className="site-layout-background">{children}</div>
       </Content>
 
