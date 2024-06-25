a3e3f08c7c8c4506be948cf339c914c46fdfb8ee
Atatakai <atatakai@Atatakais-MacBook-Pro.local>
2024-06-25 22:34:47 +0400

    Use shared footer, add contract links

--- apps/govern/components/Layout/Footer/index.tsx
+++ apps/govern/components/Layout/Footer/index.tsx
@@ -1,33 +1,63 @@
+import { Typography } from 'antd';
 import Link from 'next/link';
+import { Fragment } from 'react';
+import { mainnet } from 'viem/chains';
 
-import { Footer as CommonFooter } from '@autonolas/frontend-library';
+import { Footer as CommonFooter } from 'libs/ui-components/src';
+import { EXPLORER_URLS } from 'libs/util-constants/src';
+import { VE_OLAS, VOTE_WEIGHTING } from 'libs/util-contracts/src/lib/abiAndAddresses';
+
+import { REPO_URL } from 'common-util/constants/urls';
 
 import { Onboarding } from './Onboarding';
-import { Socials } from './Socials';
+
+const contracts = [
+  {
+    name: 'VoteWeighting',
+    link: `${EXPLORER_URLS[mainnet.id]}/address/${VOTE_WEIGHTING.addresses[mainnet.id]}`,
+  },
+  {
+    name: 'veOLAS',
+    link: `${EXPLORER_URLS[mainnet.id]}/address/${VE_OLAS.addresses[mainnet.id]}`,
+  },
+];
+
+const LeftContent = () => (
+  <Typography.Text type="secondary">
+    {`Contracts: `}
+    {contracts.map((item, index) => (
+      <Fragment>
+        {index !== 0 && ' • '}
+        <a href={item.link} target="_blank" rel="noopener noreferrer">
+          {item.name}
+        </a>
+      </Fragment>
+    ))}
+  </Typography.Text>
+);
+
+const CenterContent = () => (
+  <Typography.Text type="secondary">
+    {`© Autonolas DAO ${new Date().getFullYear()} • `}
+    <Link href="/disclaimer">Disclaimer</Link>
+    {' • '}
+    <a
+      href="https://gateway.autonolas.tech/ipfs/bafybeibrhz6hnxsxcbv7dkzerq4chssotexb276pidzwclbytzj7m4t47u"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      DAO Constitution
+    </a>
+  </Typography.Text>
+);
 
 export const Footer = () => (
-  <CommonFooter
-    rightContent={
-      <>
-        <Socials />
-        <Onboarding />
-      </>
-    }
-    centerContent={
-      <>
-        ©&nbsp;Autonolas DAO&nbsp;
-        {new Date().getFullYear()}
-        &nbsp;•&nbsp;
-        <Link href="/disclaimer">Disclaimer</Link>
-        &nbsp;•&nbsp;
-        <a
-          href="https://gateway.autonolas.tech/ipfs/bafybeibrhz6hnxsxcbv7dkzerq4chssotexb276pidzwclbytzj7m4t47u"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          DAO Constitution
-        </a>
-      </>
-    }
-  />
+  <>
+    <CommonFooter
+      leftContent={<LeftContent />}
+      centerContent={<CenterContent />}
+      githubUrl={REPO_URL}
+    />
+    <Onboarding />
+  </>
 );
