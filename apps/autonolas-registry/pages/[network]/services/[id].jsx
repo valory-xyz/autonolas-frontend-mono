3a7d23dfa8b5ee9dcdbe5b050c7330d65cbd4412
truemiller <mlxndrj@gmail.com>
2024-07-02 14:08:02 +0100

    refactor: Update dynamic import in [network]/services/[id].jsx for improved performance

--- apps/autonolas-registry/pages/[network]/services/[id].jsx
+++ apps/autonolas-registry/pages/[network]/services/[id].jsx
@@ -1,10 +1,7 @@
 import dynamic from 'next/dynamic';
 
-const ServiceDetails = dynamic(
-  () => import('../../../components/ListServices/details'),
-  {
-    ssr: false,
-  },
-);
+const ServiceDetails = dynamic(() => import('../../../components/ListServices/details'), {
+  ssr: false,
+});
 
 export default ServiceDetails;
