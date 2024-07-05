7b40bfebd18d7c0ac36c9e2de687d24d9ad2f661
mohandast52 <mohandast52@gmail.com>
2024-07-06 00:28:29 +0530

    refactor: Rename List.spec.tsx to List.test.tsx for consistency

--- apps/launch/components/MyStakingContracts/List.test.tsx
+++ apps/launch/components/MyStakingContracts/List.test.tsx
@@ -1,7 +1,6 @@
 import '@testing-library/jest-dom';
 import { render, screen, within } from '@testing-library/react';
 
-// import userEvent from '@testing-library/user-event';
 import { List } from './List';
 
 jest.mock('store/index', () => ({
