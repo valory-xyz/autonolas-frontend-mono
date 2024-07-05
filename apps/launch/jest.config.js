b0d83018f74a6901ff7f62d15940a324bcfe5636
mohandast52 <mohandast52@gmail.com>
2024-07-05 15:42:12 +0530

    chore: add launch test setup

--- apps/launch/jest.config.js
+++ apps/launch/jest.config.js
@@ -3,11 +3,10 @@ const { pathsToModuleNameMapper } = require('ts-jest');
 const { compilerOptions } = require('../../tsconfig.base.json');
 
 module.exports = {
-  displayName: 'launch',
+  displayName: 'govern',
   preset: '../../jest.preset.js',
   testEnvironment: 'jsdom',
-  moduleDirectories: ['node_modules'],
-  testTimeout: 10000,
+
   moduleNameMapper: {
     ...pathsToModuleNameMapper(compilerOptions.paths, { prefix: resolve(__dirname, '../..') }),
     '^util/(.*)$': '<rootDir>/util/$1',
@@ -16,20 +15,19 @@ module.exports = {
     '^images/(.*)$': '<rootDir>/public/images/$1',
     '^store/(.*)$': '<rootDir>/store/$1',
     '^hooks/(.*)$': '<rootDir>/hooks/$1',
+    '^types/(.*)$': '<rootDir>/types/$1',
+    '^context/(.*)$': '<rootDir>/context/$1',
   },
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
   transform: {
-    '^.+\\.[tj]sx?$': [
-      'ts-jest',
+    '^.+\\.[tj]sx?$': ['ts-jest',
       {
         presets: ['@nx/next/babel'],
         tsconfig: '<rootDir>/tsconfig.spec.json',
         useESM: true,
       },
     ],
-    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nx/react/plugins/jest',
   },
   globals: { fetch },
-  coverageDirectory: '../../coverage/apps/launch',
 };
--- apps/launch/jest.setup.js
+++ apps/launch/jest.setup.js
@@ -1,2 +1,70 @@
-import '@testing-library/jest-dom/jest-globals';
 import '@testing-library/jest-dom';
+import '@testing-library/jest-dom/jest-globals';
+
+const { TextEncoder, TextDecoder } = require('util');
+
+global.TextEncoder = TextEncoder;
+global.TextDecoder = TextDecoder;
+
+// https:// jestjs.io/docs/manual-mocks#mocking-methods-which-are-not-implemented-in-jsdom
+Object.defineProperty(window, 'matchMedia', {
+  writable: true,
+  value: jest.fn().mockImplementation((query) => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: jest.fn(), // deprecated
+    removeListener: jest.fn(), // deprecated
+    addEventListener: jest.fn(),
+    removeEventListener: jest.fn(),
+    dispatchEvent: jest.fn(),
+  })),
+});
+
+
+jest.mock('wagmi/chains', () => ({
+  gnosis: {
+    chainId: 1,
+    name: 'Gnosis',
+    symbol: 'GNO',
+    rpc: 'https://rpc.gnosis.io',
+    explorer: 'https://explorer.gnosis.io',
+    chain: 'gnosis',
+    rpcUrls: {
+      default: {
+        http: ['https://rpc.gnosischain.com'],
+        webSocket: ['wss://rpc.gnosischain.com/wss'],
+      },
+    },
+  },
+
+  mainnet: {
+    chainId: 1,
+    name: 'Ethereum',
+    symbol: 'ETH',
+    rpc: 'https://mainnet.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161',
+    explorer: 'https://etherscan.io',
+    chain: 'mainnet',
+    rpcUrls: {
+      default: {
+        http: ['https://mainnet.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161'],
+        webSocket: ['wss://mainnet.infura.io/ws/v3/9aa3d95b3bc440fa88ea12eaa4456161'],
+      },
+    },
+  },
+
+  polygon: {
+    chainId: 137,
+    name: 'Polygon',
+    symbol: 'MATIC',
+    rpc: 'https://rpc-mainnet.maticvigil.com',
+    explorer: 'https://polygonscan.com',
+    chain: 'polygon',
+    rpcUrls: {
+      default: {
+        http: ['https://rpc-mainnet.maticvigil.com'],
+        webSocket: ['wss://rpc-mainnet.maticvigil.com'],
+      },
+    },
+  }
+}));
