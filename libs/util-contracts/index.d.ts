234e08878a2bce50d65089f78f736328b7d146a7
mohandast52 <mohandast52@gmail.com>
2024-07-10 17:52:03 +0530

    chore: declare module for tokenomics.js in util-contracts

---
+++ libs/util-contracts/index.d.ts
@@ -0,0 +1,10 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+// declare module 'libs/util-contracts/src/lib/abiAndAddresses/tokenomics.js' {
+//   export const abi: any;
+//   export const addresses: any;
+// }
+
+declare module 'src/lib/abiAndAddresses/tokenomics.js' {
+  export const abi: any;
+  export const addresses: any;
+}
