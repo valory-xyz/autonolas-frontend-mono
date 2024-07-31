9fe8d8992d687037a38941941cdd5bbe0e392fbc
mohandast52 <mohandast52@gmail.com>
2024-08-01 05:10:04 +0530

    chore: add more fields

--- apps/launch/components/MyStakingContracts/Details/ContractConfiguration.tsx
+++ apps/launch/components/MyStakingContracts/Details/ContractConfiguration.tsx
@@ -1,30 +1,41 @@
 import { Row, Skeleton, Typography } from 'antd';
-import { FC, ReactNode } from 'react';
+import { FC, ReactNode, useMemo } from 'react';
 import { Address } from 'viem';
 
-import { NA } from '@autonolas/frontend-library';
+import { GATEWAY_URL, NA } from '@autonolas/frontend-library';
 
-import { EXPLORER_URLS, UNICODE_SYMBOLS } from 'libs/util-constants/src';
+import { EXPLORER_URLS, HASH_PREFIX, UNICODE_SYMBOLS } from 'libs/util-constants/src';
 import { truncateAddress } from 'libs/util-functions/src';
 
+import { CONTRACT_DEFAULT_VALUES } from 'common-util/constants/stakingContract';
 import {
+  useAgentInstances,
   useGetActivityChecker,
+  useGetAgentIds,
+  useGetConfigHash,
   useGetLivenessPeriod,
+  useGetMaximumInactivityPeriods,
   useGetMinimumStakingDeposit,
+  useGetMinimumStakingPeriods,
+  useGetMultisigThreshold,
+  useTimeForEmissions,
 } from 'hooks/useGetStakingConstants';
 import { useAppSelector } from 'store/index';
 import { MyStakingContract } from 'types/index';
 
 import {
-  ActivityCheckerAddressLabel, // AgentIdsLabel,
-  // AgentInstancesLabel,
+  ActivityCheckerAddressLabel,
+  AgentIdsLabel,
+  AgentInstancesLabel,
   LivenessPeriodLabel,
   MaximumInactivityPeriodsLabel,
   MaximumStakedAgentsLabel,
   MinimumStakingDepositLabel,
-  MinimumStakingPeriodsLabel, // MultisigThresholdLabel,
-  RewardsPerSecondLabel, // ServiceConfigHashLabel,
-  // TimeForEmissionsLabel,
+  MinimumStakingPeriodsLabel,
+  MultisigThresholdLabel,
+  RewardsPerSecondLabel,
+  ServiceConfigHashLabel,
+  TimeForEmissionsLabel,
 } from '../FormLabels';
 import { ColFlexContainer } from './helpers';
 import { useMaxNumServices, useRewardsPerSecond } from './hooks';
@@ -36,6 +47,21 @@ const ShowContent = ({ isLoading, data }: { isLoading: boolean; data?: string | 
 
   if (!networkId || isLoading) return <Skeleton.Input active size="small" />;
   return <Text>{data || NA}</Text>;
+};
+
+const ShowNetworkAddress = ({ address }: { address: Address }) => {
+  const { networkId } = useAppSelector((state) => state.network);
+
+  if (!networkId) return null;
+  if (!address) return null;
+
+  const truncatedAddress = truncateAddress(address);
+
+  return (
+    <a href={`${EXPLORER_URLS[networkId]}/address/${address}`} target="_blank" rel="noreferrer">
+      {`${truncatedAddress} ${UNICODE_SYMBOLS.EXTERNAL_LINK}`}
+    </a>
+  );
 };
 
 const MaximumStakedAgents: FC<{ address: Address }> = ({ address }) => {
@@ -54,34 +80,95 @@ const MinimumStakingDeposit: FC<{ address: Address }> = ({ address }) => {
   return <ShowContent isLoading={isLoading} data={data} />;
 };
 
+const MinimumStakingPeriods: FC<{ address: Address }> = ({ address }) => {
+  const { isLoading: isLivenessPeriodLoading, data: livenessPeriod } = useGetLivenessPeriod({
+    address,
+  });
+  const { isLoading: isMinimumStakingPeriodsLoading, data: minimumStakingPeriods } =
+    useGetMinimumStakingPeriods({ address });
+
+  const isLoading = useMemo(
+    () => isLivenessPeriodLoading || isMinimumStakingPeriodsLoading,
+    [isLivenessPeriodLoading, isMinimumStakingPeriodsLoading],
+  );
+  const data = useMemo(() => {
+    if (!livenessPeriod) return NA;
+    if (!minimumStakingPeriods) return NA;
+
+    const minStakingDuration = Number(minimumStakingPeriods) / Number(livenessPeriod);
+    return `${minStakingDuration}`;
+  }, [livenessPeriod, minimumStakingPeriods]);
+
+  return <ShowContent isLoading={isLoading} data={data} />;
+};
+
+const MaximumInactivityPeriods: FC<{ address: Address }> = ({ address }) => {
+  const { data, isLoading } = useGetMaximumInactivityPeriods({ address });
+  return <ShowContent isLoading={isLoading} data={data} />;
+};
+
 const LivenessPeriod: FC<{ address: Address }> = ({ address }) => {
   const { data, isLoading } = useGetLivenessPeriod({ address });
   return <ShowContent isLoading={isLoading} data={data ? `${data} seconds` : NA} />;
 };
 
-const ActivityCheckerAddress: FC<{ address: Address }> = ({ address }) => {
-  const { networkId } = useAppSelector((state) => state.network);
-  const { data: checkerAddress, isLoading } = useGetActivityChecker({ address });
-  const truncatedAddress = checkerAddress ? truncateAddress(checkerAddress) : NA;
+const TimeForEmissions: FC<{ address: Address }> = ({ address }) => {
+  const { data, isLoading } = useTimeForEmissions({ address });
+  return <ShowContent isLoading={isLoading} data={data} />;
+};
 
+const AgentInstances: FC<{ address: Address }> = ({ address }) => {
+  const { data, isLoading } = useAgentInstances({ address });
+  return <ShowContent isLoading={isLoading} data={data} />;
+};
+
+const AgentIds: FC<{ address: Address }> = ({ address }) => {
+  const { data, isLoading } = useGetAgentIds({ address });
+  return (
+    <ShowContent isLoading={isLoading} data={!data || data?.length === 0 ? NA : data.join(', ')} />
+  );
+};
+
+const MultisigThreshold: FC<{ address: Address }> = ({ address }) => {
+  const { data, isLoading } = useGetMultisigThreshold({ address });
+  return <ShowContent isLoading={isLoading} data={data} />;
+};
+
+const ConfigHash: FC<{ address: Address }> = ({ address }) => {
+  const { data: configHash, isLoading } = useGetConfigHash({ address });
+  const isZeroAddress = configHash === CONTRACT_DEFAULT_VALUES.configHash;
+
+  const getConfigHash = () => {
+    if (!configHash) return NA;
+
+    const truncateConfigHash = truncateAddress(configHash);
+    if (isZeroAddress) return truncateConfigHash;
+
+    const uri = `${HASH_PREFIX}${configHash.substring(2)}`;
+    const ipfsUrl = `${GATEWAY_URL}${uri}`;
+    return (
+      <a href={ipfsUrl} target="_blank" rel="noreferrer">
+        {truncateConfigHash} {UNICODE_SYMBOLS.EXTERNAL_LINK}
+      </a>
+    );
+  };
+
+  return <ShowContent isLoading={isLoading} data={getConfigHash()} />;
+};
+
+const ActivityCheckerAddress: FC<{ address: Address }> = ({ address }) => {
+  const { data: checkerAddress, isLoading } = useGetActivityChecker({ address });
   return (
     <ShowContent
       isLoading={isLoading}
-      data={
-        networkId ? (
-          <a
-            href={`${EXPLORER_URLS[networkId]}/address/${checkerAddress}`}
-            target="_blank"
-            rel="noreferrer"
-          >
-            {`${truncatedAddress} ${UNICODE_SYMBOLS.EXTERNAL_LINK}`}
-          </a>
-        ) : null
-      }
+      data={<ShowNetworkAddress address={checkerAddress as Address} />}
     />
   );
 };
 
+/**
+ * contract configuration details component for details page
+ */
 export const ContractConfiguration: FC<{ myStakingContract: MyStakingContract }> = ({
   myStakingContract,
 }) => {
@@ -103,29 +190,50 @@ export const ContractConfiguration: FC<{ myStakingContract: MyStakingContract }>
           text={<MinimumStakingDepositLabel />}
           content={<MinimumStakingDeposit address={myStakingContract.id} />}
         />
-        <ColFlexContainer text={<MinimumStakingPeriodsLabel />} content={<>TODO</>} />
+        <ColFlexContainer
+          text={<MinimumStakingPeriodsLabel />}
+          content={<MinimumStakingPeriods address={myStakingContract.id} />}
+        />
       </Row>
 
       <Row gutter={24}>
-        <ColFlexContainer text={<MaximumInactivityPeriodsLabel />} content={<>TODO</>} />
+        <ColFlexContainer
+          text={<MaximumInactivityPeriodsLabel />}
+          content={<MaximumInactivityPeriods address={myStakingContract.id} />}
+        />
         <ColFlexContainer
           text={<LivenessPeriodLabel />}
           content={<LivenessPeriod address={myStakingContract.id} />}
         />
       </Row>
 
-      {/* <Row gutter={24}>
-        <ColFlexContainer text={<TimeForEmissionsLabel />} content={<>TODO</>} />
-        <ColFlexContainer text={<AgentInstancesLabel />} content={<>TODO</>} />
-      </Row> */}
-
-      {/* <Row gutter={24}>
-        <ColFlexContainer text={<AgentIdsLabel />} content={<>TODO</>} />
-        <ColFlexContainer text={<MultisigThresholdLabel />} content={<>TODO</>} />
-      </Row> */}
+      <Row gutter={24}>
+        <ColFlexContainer
+          text={<TimeForEmissionsLabel />}
+          content={<TimeForEmissions address={myStakingContract.id} />}
+        />
+        <ColFlexContainer
+          text={<AgentInstancesLabel />}
+          content={<AgentInstances address={myStakingContract.id} />}
+        />
+      </Row>
 
       <Row gutter={24}>
-        {/* <ColFlexContainer text={<ServiceConfigHashLabel />} content={<>TODO</>} /> */}
+        <ColFlexContainer
+          text={<AgentIdsLabel />}
+          content={<AgentIds address={myStakingContract.id} />}
+        />
+        <ColFlexContainer
+          text={<MultisigThresholdLabel />}
+          content={<MultisigThreshold address={myStakingContract.id} />}
+        />
+      </Row>
+
+      <Row gutter={24}>
+        <ColFlexContainer
+          text={<ServiceConfigHashLabel />}
+          content={<ConfigHash address={myStakingContract.id} />}
+        />
         <ColFlexContainer
           text={<ActivityCheckerAddressLabel />}
           content={<ActivityCheckerAddress address={myStakingContract.id} />}
--- apps/launch/hooks/useGetStakingConstants.ts
+++ apps/launch/hooks/useGetStakingConstants.ts
@@ -6,14 +6,16 @@ import { STAKING_TOKEN } from 'libs/util-contracts/src/lib/abiAndAddresses';
 
 import { useAppSelector } from 'store/index';
 
-const useStakingContractConstant = ({
+const useStakingContractConstant = <T>({
   name,
   address,
+  args,
   select,
 }: {
   address: Address;
   name: string;
-  select?: (data: unknown) => string;
+  args?: unknown[];
+  select?: (data: unknown) => T;
 }) => {
   const { networkId } = useAppSelector((state) => state.network);
 
@@ -22,9 +24,15 @@ const useStakingContractConstant = ({
     abi: STAKING_TOKEN.abi,
     chainId: networkId as number,
     functionName: name,
+    args,
     query: {
       enabled: !!networkId && !!address && !!name,
-      select: select || ((data) => (typeof data === 'bigint' ? data.toString() : '0')),
+      select:
+        select ||
+        (((data) => {
+          if (typeof data === 'bigint') return data.toString();
+          return data || '0';
+        }) as (data: unknown) => T),
     },
   });
 };
@@ -37,17 +45,38 @@ export const useGetMinimumStakingDeposit = ({ address }: { address: Address }) =
   });
 
 export const useGetMinimumStakingPeriods = ({ address }: { address: Address }) =>
-  useStakingContractConstant({ address, name: 'minStakingPeriods' });
+  useStakingContractConstant<string>({ address, name: 'minStakingDuration' });
+
+export const useGetMaximumInactivityPeriods = ({ address }: { address: Address }) =>
+  useStakingContractConstant<string>({ address, name: 'maxNumInactivityPeriods' });
 
 export const useGetLivenessPeriod = ({ address }: { address: Address }) =>
-  useStakingContractConstant({ address, name: 'livenessPeriod' });
+  useStakingContractConstant<string>({ address, name: 'livenessPeriod' });
+
+export const useTimeForEmissions = ({ address }: { address: Address }) =>
+  useStakingContractConstant<string>({ address, name: 'timeForEmissions' });
+
+export const useAgentInstances = ({ address }: { address: Address }) =>
+  useStakingContractConstant<string>({ address, name: 'numAgentInstances' });
+
+export const useGetAgentIds = ({ address }: { address: Address }) =>
+  useStakingContractConstant<string[]>({
+    address,
+    name: 'getAgentIds',
+    select: (data) => {
+      const ids = data as bigint[];
+      return ids.map((id) => id.toString());
+    },
+  });
+
+export const useGetMultisigThreshold = ({ address }: { address: Address }) =>
+  useStakingContractConstant<string>({ address, name: 'threshold' });
+
+export const useGetConfigHash = ({ address }: { address: Address }) =>
+  useStakingContractConstant<string>({ address, name: 'configHash' });
 
 export const useGetActivityChecker = ({ address }: { address: Address }) =>
-  useStakingContractConstant({
-    address,
-    name: 'activityChecker',
-    select: (data) => data as string,
-  });
+  useStakingContractConstant({ address, name: 'activityChecker' });
 
 // export const useGetLivenessPeriod = ({ address }: { address: Address }) => {
 //   const { networkId } = useAppSelector((state) => state.network);
