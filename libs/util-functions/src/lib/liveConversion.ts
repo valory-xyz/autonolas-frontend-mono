0862535dc8f4aa47f0e5fce72b270d830367fb32
mohandast52 <mohandast52@gmail.com>
2024-09-03 18:22:39 +0530

    feat: add cache to conversion

--- libs/util-functions/src/lib/liveConversion.ts
+++ libs/util-functions/src/lib/liveConversion.ts
@@ -1,17 +1,27 @@
+const cache = new Map<number, number>();
+
 export async function convertUsdToEth(
   usdAmount: number,
   roundTo: number = 4,
 ): Promise<number | null> {
+  if (cache.has(usdAmount)) {
+    return cache.get(usdAmount) as number;
+  }
+
   try {
     const response = await fetch(
       'https://api.coingecko.com/api/v3/simple/price?ids=ethereum&vs_currencies=usd',
     );
     const data = await response.json();
 
-    const ethRate = data.ethereum.usd; // USD to ETH rate
+    const ethRate = data.ethereum.usd;
     const ethAmount = usdAmount / ethRate;
+    const ethAmountRounded = Number(ethAmount.toFixed(roundTo));
 
-    return Number(ethAmount.toFixed(roundTo));
+    // Cache the result
+    cache.set(usdAmount, ethAmountRounded);
+
+    return ethAmountRounded;
   } catch (error) {
     throw new Error('Error fetching conversion rate');
   }
