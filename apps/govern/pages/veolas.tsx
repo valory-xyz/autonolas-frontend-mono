87a4e44abf04da67328f03b03955c3548ac36fd4
atepem <57289722+atepem@users.noreply.github.com>
2024-11-14 12:34:14 +0000

    chore: fixed title for veOLAS

--- apps/govern/pages/veolas.tsx
+++ apps/govern/pages/veolas.tsx
@@ -4,7 +4,7 @@ import { VeOlas } from '../components/VeOlas';
 const VeOlasPage = () => (
   <>
     <Meta
-      pageTitle="veOlas"
+      pageTitle="veOLAS"
       description="Easily check, manage, and optimize your veOLAS balance to actively participate in decisions shaping the future of the Olas network."
       pageUrl="veOlas"
     />
