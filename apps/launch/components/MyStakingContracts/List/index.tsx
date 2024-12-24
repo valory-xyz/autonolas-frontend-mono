bec8913405f37fb580cd179199203f5f553d9cf4
Atatakai <densatsu@mail.ru>
2024-12-24 12:39:19 +0400

    (launch) feat: add scan link with copy-to-clipboard button

--- apps/launch/components/MyStakingContracts/List/index.tsx
+++ apps/launch/components/MyStakingContracts/List/index.tsx
@@ -1,16 +1,17 @@
-import { CheckCircleOutlined, InfoCircleOutlined } from '@ant-design/icons';
-import { Alert, Button, Flex, Popover, Table, Tag, Typography } from 'antd';
+import { CheckCircleOutlined, CopyOutlined, InfoCircleOutlined } from '@ant-design/icons';
+import { Alert, Button, Flex, message, Popover, Table, Tag, Typography } from 'antd';
 import type { TableProps } from 'antd';
 import Link from 'next/link';
 import React, { FC, useMemo } from 'react';
 
-import { GOVERN_URL } from 'libs/util-constants/src';
+import { EXPLORER_URLS, GOVERN_URL } from 'libs/util-constants/src';
+import { UNICODE_SYMBOLS } from 'libs/util-constants/src/lib/symbols';
 
 import { URL } from 'common-util/constants/urls';
 import { useAppSelector } from 'store/index';
 import { MyStakingContract } from 'types/index';
 
-const { Paragraph } = Typography;
+const { Paragraph, Text } = Typography;
 
 const NominatedForIncentivesPopover = () => (
   <Popover
@@ -32,7 +33,7 @@ const NominatedForIncentivesPopover = () => (
 );
 
 const useColumns = () => {
-  const { networkName } = useAppSelector((state) => state.network);
+  const { networkName, networkId } = useAppSelector((state) => state.network);
 
   const columns: TableProps<MyStakingContract>['columns'] = useMemo(
     () => [
@@ -42,9 +43,29 @@ const useColumns = () => {
         key: 'name',
         width: '25%',
         render: (text, record) => (
-          <Link href={`/${networkName}/${URL.myStakingContracts}/${record.id}`} passHref>
-            {text}
-          </Link>
+          <Flex vertical gap={4}>
+            <Link href={`/${networkName}/${URL.myStakingContracts}/${record.id}`}>{text}</Link>
+            {networkId && (
+              <Flex gap={12}>
+                <a
+                  href={`${EXPLORER_URLS[networkId]}/address/${record.id}`}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <Text type="secondary">View on explorer {UNICODE_SYMBOLS.EXTERNAL_LINK}</Text>
+                </a>
+                <Button
+                  onClick={() =>
+                    navigator.clipboard
+                      .writeText(`${EXPLORER_URLS[networkId]}/address/${record.id}`)
+                      .then(() => message.success('Link copied!'))
+                  }
+                  size="small"
+                  icon={<CopyOutlined style={{ fontSize: 12 }} />}
+                />
+              </Flex>
+            )}
+          </Flex>
         ),
       },
       {
