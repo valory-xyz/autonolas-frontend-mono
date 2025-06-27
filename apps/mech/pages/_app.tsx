d39e2677e6bf9ce9488d95a64e2a2694faea081f
Rajat Verma <rajatverma5885045@gmail.com>
2025-06-27 15:44:05 +0530

    feat: working setup

--- apps/mech/pages/_app.tsx
+++ apps/mech/pages/_app.tsx
@@ -1,6 +1,6 @@
-import { createWrapper } from 'next-redux-wrapper';
 import { AppProps } from 'next/app';
 import Head from 'next/head';
+import { Provider } from 'react-redux';
 
 import { METADATA } from 'common-util/Login/config';
 import GlobalStyle from 'components/GlobalStyles';
@@ -8,24 +8,29 @@ import Layout from 'components/Layout';
 import { AutonolasThemeProvider } from 'context/AutonolasThemeProvider';
 import { Web3ModalProvider } from 'context/Web3ModalProvider';
 
-import initStore from '../store';
+import { wrapper } from '../store';
 
-const MyApp = ({ Component, pageProps }: AppProps) => (
-  <>
-    <GlobalStyle />
-    <Head>
-      <title>{METADATA.name}</title>
-      <meta name="description" content={METADATA.description} />
-    </Head>
-    <AutonolasThemeProvider>
-      <Web3ModalProvider>
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
-      </Web3ModalProvider>
-    </AutonolasThemeProvider>
-  </>
-);
+const MyApp = ({ Component, pageProps }: AppProps) => {
+  const { store, props } = wrapper.useWrappedStore(pageProps);
 
-const wrapper = createWrapper(initStore);
-export default wrapper.withRedux(MyApp);
+  return (
+    <>
+      <GlobalStyle />
+      <Head>
+        <title>{METADATA.name}</title>
+        <meta name="description" content={METADATA.description} />
+      </Head>
+      <Provider store={store}>
+        <AutonolasThemeProvider>
+          <Web3ModalProvider>
+            <Layout>
+              <Component {...props.pageProps} />
+            </Layout>
+          </Web3ModalProvider>
+        </AutonolasThemeProvider>
+      </Provider>
+    </>
+  );
+};
+
+export default MyApp;
--- apps/mech/store/index.js
+++ apps/mech/store/index.js
@@ -1,21 +1,12 @@
-import { applyMiddleware, combineReducers, compose, createStore } from 'redux';
-import thunk from 'redux-thunk';
+import { createWrapper } from 'next-redux-wrapper';
+import { configureStore } from '@reduxjs/toolkit';
 
 import setup from './setup';
 
-const rootReducer = combineReducers({ setup });
+export const store = configureStore({
+  reducer: {
+    setup,
+  },
+});
 
-const enhancers = [];
-const middleware = [thunk];
-
-const composeWithDevTools =
-  typeof window === 'object' &&
-  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ &&
-  process.env.NODE_ENV === 'development'
-    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({})
-    : compose;
-
-const composedEnhancers = composeWithDevTools(applyMiddleware(...middleware), ...enhancers);
-
-const Store = () => createStore(rootReducer, composedEnhancers);
-export default Store;
+export const wrapper = createWrapper(() => store);
