a35699b9a9fcd4678fa5ac3f187544ae3c626f63
atepem <57289722+atepem@users.noreply.github.com>
2025-08-26 18:21:19 +0100

    chore: https urls

--- apps/docs/util/constants.ts
+++ apps/docs/util/constants.ts
@@ -1,12 +1,12 @@
 export const OLAS_SITE_URL = 'https://olas.network/';
 export const OPERATOR_NAME = 'Olas (aka Autonolas) DAO';
 
-export const BUILD_SITE_URL = 'build.olas.network';
-export const CONTRIBUTE_SITE_URL = 'contribute.olas.network';
-export const OPERATE_SITE_URL = 'operate.olas.network';
-export const BOND_SITE_URL = 'bond.olas.network';
-export const GOVERN_SITE_URL = 'govern.olas.network';
-export const LAUNCH_SITE_URL = 'launch.olas.network';
+export const BUILD_SITE_URL = 'https://build.olas.network';
+export const CONTRIBUTE_SITE_URL = 'https://contribute.olas.network';
+export const OPERATE_SITE_URL = 'https://operate.olas.network';
+export const BOND_SITE_URL = 'https://bond.olas.network';
+export const GOVERN_SITE_URL = 'https://govern.olas.network';
+export const LAUNCH_SITE_URL = 'https://launch.olas.network';
 
 export const X_URL = 'https://x.com/autonolas';
 export const YOUTUBE_URL = 'https://www.youtube.com/@autonolas';
