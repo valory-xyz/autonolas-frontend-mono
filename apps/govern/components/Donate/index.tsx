1d735a890fb7e977e39d91b5c4287dd1de4ed7a2
mohandast52 <mohandast52@gmail.com>
2025-09-04 14:54:26 +0530

    fix: temporarily hide the checkpoint button in the DonatePage component

--- apps/govern/components/Donate/index.tsx
+++ apps/govern/components/Donate/index.tsx
@@ -193,7 +193,6 @@ export const DonatePage = () => {
             loading={isCheckpointLoading}
             disabled={!account || isDataLoading || isExpectedEndTimeInFuture}
             onClick={onCheckpoint}
-            style={{ display: 'none' }} // TODO: temporarily hide this button
           >
             Start new epoch
           </Button>
