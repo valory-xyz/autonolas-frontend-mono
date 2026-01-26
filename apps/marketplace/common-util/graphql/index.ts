3de5fd8fabc7b71822b2d581fc1f04ae0e2e3783
Atatakai <densatsu@mail.ru>
2026-01-26 19:08:19 +0100

    feat: add marketplace activity on polygon and optimism

--- apps/marketplace/common-util/graphql/index.ts
+++ apps/marketplace/common-util/graphql/index.ts
@@ -10,6 +10,8 @@ const requestConfig: RequestConfig = {
 };
 
 export const MARKETPLACE_SUBGRAPH_CLIENTS = {
+  10: new GraphQLClient(process.env.NEXT_PUBLIC_OPTIMISM_MARKETPLACE_SUBGRAPH!, requestConfig),
   100: new GraphQLClient(process.env.NEXT_PUBLIC_GNOSIS_MARKETPLACE_SUBGRAPH!, requestConfig),
+  137: new GraphQLClient(process.env.NEXT_PUBLIC_POLYGON_MARKETPLACE_SUBGRAPH!, requestConfig),
   8453: new GraphQLClient(process.env.NEXT_PUBLIC_BASE_MARKETPLACE_SUBGRAPH!, requestConfig),
 };
