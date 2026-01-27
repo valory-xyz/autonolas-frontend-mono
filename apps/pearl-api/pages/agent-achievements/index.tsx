84ae95b95c51e9e75888e692c10ed1c15a613f3e
Rajat Verma <rajatverma5885045@gmail.com>
2026-01-27 23:39:17 +0530

    fix: build

--- apps/pearl-api/pages/agent-achievements/index.tsx
+++ apps/pearl-api/pages/agent-achievements/index.tsx
@@ -1,17 +1,28 @@
+import { useEffect, useState } from 'react';
 import { AchievementUI } from '../../components/Achievement/AchievementUI';
 import { OG_IMAGE_CONFIG } from '../../constants/achievement';
-import { AchievementQueryParams } from '../../types/achievement';
+import { AchievementQueryParams, PolymarketBetData } from '../../types/achievement';
+import { getPolymarketBet } from 'utils/polymarket';
 
 export default function AgentAchievements() {
+  const [data, setData] = useState<PolymarketBetData | null>(null);
+    
   const params: AchievementQueryParams = {
     agent: 'polystrat',
     type: 'payout',
-    id: '123',
+    id: '0x74bfbf071a414817d27bf8d098a883a6be925425a3d5fb1ae4097f8bb0593ca498020000',
   };
 
   // Determine the image src for client-side
   const logoSrc = '/images/polystrat-logo.png';
 
+  useEffect(() => {
+    getPolymarketBet(params.id).then((data) => {
+      setData(data);
+    });
+  }, [params.id]);
+
+  if(!data) return null;
   return (
     <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
       <div
@@ -20,7 +31,7 @@ export default function AgentAchievements() {
           height: OG_IMAGE_CONFIG.HEIGHT,
         }}
       >
-        <AchievementUI params={params} logoSrc={logoSrc} />
+        <AchievementUI params={params} logoSrc={logoSrc} data={data} />
       </div>
     </div>
   );
