77042c7f9b29416ae8e3857c01df33aa33b8e704
Rajat Verma <rajatverma5885045@gmail.com>
2026-02-04 16:44:23 +0530

    feat: use polygon subgraph for erc8004 fields (#297)

--- libs/util-contracts/src/lib/abiAndAddresses/identityRegistryUpgradeable.ts
+++ libs/util-contracts/src/lib/abiAndAddresses/identityRegistryUpgradeable.ts
@@ -2,5 +2,6 @@ export const IDENTITY_REGISTRY_UPGRADEABLE = {
   contractName: "IdentityRegistryUpgradeable", // ERC1967Proxy
   addresses: {
     1: "0x8004a169fb4a3325136eb29fa0ceb6d2e539a432",
+    137: "0x8004a169fb4a3325136eb29fa0ceb6d2e539a432",
   },
 };
